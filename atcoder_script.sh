23ccb47cab45a83659731a0598f6e35c2036622a
Yuki Ise <70761052+Benjamin-taro@users.noreply.github.com>
2025-04-28 15:30:13 +0100

    hello

--- atcoder_script.sh
+++ atcoder_script.sh
@@ -4,3 +4,8 @@ cd /Users/iseyuki/atcoder.jp  # リポジトリのパスに移動
 git add .
 git commit -m "Automated commit"
 git push origin main
+
+cd /Users/iseyuki/programming\ practice/codeforces  
+git add .
+git commit -m "Automated commit"
+git push origin main
